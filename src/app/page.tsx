b33edd5dc9a4ef3034f7ac6a0fd241ad39472311
Sulaiman Sabry <sulaiman.20222190@iit.ac.lk>
2026-01-06 01:21:32 +0530

    Refactor: Update color palette to Zinc and fix dark mode background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { QuoteCalculator } from "@/components/QuoteCalculator";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50">
+    <main className="min-h-screen">
       <QuoteCalculator />
     </main>
   );
